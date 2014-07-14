d42d3b687fc5d7c6fe5ad67343b58561616c0dd1
Bojan Hribernik <bojan.hribernik@gmail.com>
2014-07-14 05:10:06 +0200

    Work in progress ...

--- packages/cb-paper-core/src/Transform.js
+++ packages/cb-paper-core/src/Transform.js
@@ -4,10 +4,20 @@
 Ext.define('CB.paper.Transform', {
     
     applyTransform: function() {
+        if (Ext.supports.Css3DTransforms) {
+            this.applyTransform3D();
+        } else if (Ext.supports.CssTransforms) {
+            this.applyTransform2D();
+        } else {
+            this.applyTransform1D();
+        }
+    },
+    
+    applyTransform3D: function() {
         var scale = this.getScale(),
             x = this.getTranslateX(),
             y = this.getTranslateY(),
-            transform = 'translate3d(' + x + 'px, ' + y + 'px, 0) scale3d(' + scale + ',' + scale + ',1)',
+            transform = 'translate3d(' + x + 'px, ' + y + 'px, 0) scale3d(' + scale + ',' + scale + ', 1)',
             style = {
                 'transform': transform,
                 '-o-transform': transform,
@@ -16,6 +26,37 @@ Ext.define('CB.paper.Transform', {
                 '-webkit-transform': transform
             };
     
+        this.getImage().setStyle(style);
+    },
+    
+    applyTransform2D: function() {
+        var scale = this.getScale(),
+            x = this.getTranslateX(),
+            y = this.getTranslateY(),
+            transform = 'translate(' + x + 'px, ' + y + 'px) scale(' + scale + ',' + scale + ')',
+            style = {
+                'transform': transform,
+                '-o-transform': transform,
+                '-ms-transform': transform,
+                '-moz-transform': transform,
+                '-webkit-transform': transform
+            };
+    
+        this.getImage().setStyle(style);
+    },
+    
+    applyTransform1D: function() {
+        var width = this.getImageScaledWidth() + 'px',
+            height = this.getImageScaledHeight() + 'px',
+            top = this.getTranslateY() + 'px',
+            left = this.getTranslateX() + 'px',
+            style = {
+                width: width,
+                height: height,
+                top: top,
+                left: left
+            };
+            
         this.getImage().setStyle(style);
     }
 
